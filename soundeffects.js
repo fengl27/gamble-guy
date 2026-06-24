3e4a44be4c49ae823541127d8e9b69ed86e2070d
fengl27 <lufeng312@outlook.com>
2026-06-24 08:39:57 -0400

    i forgor to do something

--- soundeffects.js
+++ soundeffects.js
@@ -634,14 +634,11 @@ var otherSounds = {
     buy: "buy.mp3",
 
     bubble: "bubble.mp3",//for potion
-<<<<<<< HEAD
 
     shieldBreak: "shield-break.mp3",
     shieldUse: "shield-use.mp3",
-=======
     
     parry: "parry.mp3"
->>>>>>> 21e879b76e6565df4c7ec123058bcd15985acf2c
 };
 var music = {
     playing: "song.mp3",
